e4df6c88a128e0f36d993841ba47af0089746cb1
Loïc Mura <batlolo.1999@hotmail.fr>
2015-09-09 19:19:03 +0200

    Add more security
    API keys check added
    force-ssl package added to use https

--- server/api.js
+++ server/api.js
@@ -1,22 +1,30 @@
 Router.route('/achats', function() {
 	body = this.request.body;
+	var key = body.key; //API key
 	var type = body.type; //Tickets ou abonnements
 	var email = body.email; //Email de l'acheteur
 	var amount = body.amount; //Nombre acheté
 
-	if(email && amount) if(type == "tickets") {
-		console.log(email + ' bought ' + amount + ' tickets.');
-		Meteor.call('buyTickets', email, amount);
-	} else if(type == "abo") {
-		console.log(email + ' bought ' + amount + ' abo.');
-		Meteor.call('buyAbo', email, amount);
-	} else console.log('A mistake hapenned !');
+	if(key && key =="write_your_key_here")
+	{
+		if(email && amount) if(type == "tickets") {
+			console.log(email + ' bought ' + amount + ' tickets.');
+			Meteor.call('buyTickets', email, amount);
+		} else if(type == "abo") {
+			console.log(email + ' bought ' + amount + ' abo.');
+			Meteor.call('buyAbo', email, amount);
+		} else console.log('A mistake hapenned !');
+	} else console.log('Acces refused')
 }, {where: 'server'});
 
 Router.route('/presence', function() {
 	body = this.request.body;
+	var key = body.key; //API key
 	var email = body.email; //email de la personne
 
-	if(email)Meteor.call('addPresence', email);
-	else console.log('A mistake happened !');
+	if(key && key =="write_your_other_key_here") 
+	{
+		if(email) Meteor.call('addPresence', email);
+		else console.log('A mistake happened !');
+	} else console.log('Acces refused')
 }, {where: 'server'});
