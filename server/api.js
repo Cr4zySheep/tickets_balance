dc1529d5e83dc5acaf3558fafd03eab4698426cb
Quentin Bonnard <quentin.bonnard+github@gmail.com>
2015-09-17 01:01:46 +0200

    Format and remove lints in api.js

--- server/api.js
+++ server/api.js
@@ -1,30 +1,42 @@
 Router.route('/achats', function() {
-	body = this.request.body;
+	var body = this.request.body;
 	var key = body.key; //API key
 	var type = body.type; //Tickets ou abonnements
 	var email = body.email; //Email de l'acheteur
 	var amount = body.amount; //Nombre acheté
 
-	if(key && key =="write_your_key_here")
+	if(key && key === "your_key")
 	{
-		if(email && amount) if(type == "tickets") {
-			console.log(email + ' bought ' + amount + ' tickets.');
-			Meteor.call('buyTickets', email, amount);
-		} else if(type == "abo") {
-			console.log(email + ' bought ' + amount + ' abo.');
-			Meteor.call('buyAbo', email, amount);
-		} else console.log('A mistake hapenned !');
-	} else console.log('Acces refused')
+		if(email && amount) {
+            if(type === "tickets") {
+                console.log(email + ' bought ' + amount + ' tickets.');
+                Meteor.call('buyTickets', email, amount);
+            } else if(type === "abo") {
+                console.log(email + ' bought ' + amount + ' abo.');
+                Meteor.call('buyAbo', email, amount);
+            } else {
+                console.log('An error hapenned !');
+            }
+        }
+	} else {
+        console.log('Access denied');
+    }
 }, {where: 'server'});
 
 Router.route('/presence', function() {
-	body = this.request.body;
+	var body = this.request.body;
 	var key = body.key; //API key
 	var email = body.email; //email de la personne
 
-	if(key && key =="write_your_other_key_here") 
+	if(key && key === "your_key")
 	{
-		if(email) Meteor.call('addPresence', email);
-		else console.log('A mistake happened !');
-	} else console.log('Acces refused')
+		if(email) {
+            Meteor.call('addPresence', email);
+        }
+		else {
+            console.log('A mistake happened !');
+        }
+	} else {
+        console.log('Acces refused');
+    }
 }, {where: 'server'});
