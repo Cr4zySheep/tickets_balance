d058098a90fe36fb9193f0c3b24f741c92095fb1
Loïc Mura <batlolo.1999@hotmail.fr>
2015-09-19 19:20:34 +0200

    Remove error when no user are logged in

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 //Only on the server
 //Contains all the purchase
 Meteor.publish("purchase", function() {
-  	return purchase.find({owner: Meteor.users.findOne(this.userId).emails[0].address});
+  	if(this.userId) return purchase.find({owner: Meteor.users.findOne(this.userId).emails[0].address});
+    else return;
 });
 
 Meteor.publish("presence", function() {
-	return presence.find({owner: Meteor.users.findOne(this.userId).emails[0].address});
+	if(this.userId) return presence.find({owner: Meteor.users.findOne(this.userId).emails[0].address});
+  else return;
 });
