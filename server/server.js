cf81a72ea09301471e9a790fde44a8a84c1828d5
Quentin Bonnard <quentin.bonnard+github@gmail.com>
2015-09-29 00:10:04 +0200

    Show all data only to admins

--- server/server.js
+++ server/server.js
@@ -2,15 +2,29 @@ var Meteor = this.Meteor;
 var purchase = this.purchase;
 var presence = this.presence;
 
+function isAdmin(userId) {
+    var user = Meteor.users.findOne({_id: userId});
+    return user && user.profile && user.profile.isAdmin;
+}
+
+// db.users.update({_id:""},{$set:{"profile.isAdmin":true}})
 Meteor.publish("allUsers", function() {
-    //TODO only for admins
-    return Meteor.users.find();
+    if (isAdmin(this.userId)) {
+        return Meteor.users.find();
+    }
+    return [];
 });
 
 Meteor.publish("purchase", function(userId) {
-    return purchase.find({userId: userId});
+    if (isAdmin(this.userId) || userId === this.userId) {
+        return purchase.find({userId: userId});
+    }
+    return [];
 });
 
 Meteor.publish("presence", function(userId) {
-    return presence.find({userId: userId});
+    if (isAdmin(this.userId) || userId === this.userId) {
+        return presence.find({userId: userId});
+    }
+    return [];
 });
